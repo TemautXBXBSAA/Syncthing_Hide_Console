802033b63d9a96e6ff3000103dba204a57500912
TemautXBXBSAA <72258267+TemautXBXBSAA@users.noreply.github.com>
2026-08-28 12:56:22 +0800

    Update tray.py

--- src/tray.py
+++ src/tray.py
@@ -4,8 +4,12 @@ from pystray import MenuItem,Icon
 from PIL import Image
 from typing import Callable
 
-from . import change_path
-from .log import Logger,INFO
+try:
+    from . import change_path
+    from .log import Logger,INFO
+except ImportError:
+    from log import Logger,INFO
+    import change_path
 
 logger = Logger("pystray","log.log")
 logger.setLevel(INFO)
@@ -33,7 +37,6 @@ class TrayApp:
         logger.info(f"{self.name} starting tray icon thread")
         self.thread = threading.Thread(target=self.__start__)
         self.thread.start()
-        
 
     def add_exit(self,callback:Callable=lambda x=1:x):
         logger.info(f"{self.name} adding exit menu item")
@@ -49,9 +52,16 @@ class TrayApp:
         self.on_exit()
         if self.icon:
             self.icon.stop()
-    def add_menu(self,menu_name:str,menu_func:Callable):
+    def add_menu(self,menu_name:str,menu_func:Callable,checked = None,radio: bool = False,default: bool = False,visible: bool = True,enabled: bool = True):
         logger.debug(f"{self.name} adding menu item: {menu_name}")
-        self.menu.append(MenuItem(menu_name,menu_func))
+        self.menu.append(
+            MenuItem(menu_name,
+                     menu_func,
+                     checked=checked,
+                     radio=radio,
+                     default=default,
+                     visible=visible,
+                     enabled=enabled))
         self.menu_name.append(menu_name)
     
     def add_menus(self,*menu_item):
@@ -65,4 +75,4 @@ class TrayApp:
                 self.menu.append(item)
                 self.menu_name.append(item.__name__)
     def __str__(self):
-        return (f"<TrayApp: {self.menu_name}>")
+        return (f"<{self.name}: {self.menu_name}>")
